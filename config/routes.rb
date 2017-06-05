a952c237b423c33e31927ec6c9644e6f45950a87
thkocks <timhkocks@gmail.com>
2017-06-05 20:26:25 +0200

    Add froms

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   devise_for :users
+
+  resources :profiles, only: [:new, :edit, :create, :update]
+  
+  resources :songs
+  resources :artists
+  resources :albums
+
 end
